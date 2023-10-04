e5372be887e16e4f4e001b38e27a4a7740e6afc4
Gussia835 <cerberweter@gmail.com>
2023-10-04 20:17:20 +0300

    changed hello

--- hello.pas
+++ hello.pas
@@ -1,5 +1,10 @@
 ﻿begin
-  Print('Hello world!');
+  var language := Readstring('на каком языке ты говоришь?'); //спрашивает язык на котором человек говорит
+  case language of
+  'english':  Println('Hello!'); //выводит привет на английском  
+  'русский':  Println('Привет!'); //выводит привет на русском  
+  'deutsch': Println('Hallo!');  //выводит привет на немецком
+end;
 end.
 
 
